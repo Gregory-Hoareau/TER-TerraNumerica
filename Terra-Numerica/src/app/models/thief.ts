5e50624676ca9202998af30719dec3da1e88e1d7
AnthonyChoquard <anthony.choquard@etu.univ-cotedazur.fr>
2020-11-19 16:12:09 +0100

    ajout des model pions #2
    model pions voleur et policier

---
+++ Terra-Numerica/src/app/models/thief.ts
@@ -0,0 +1,26 @@
+import { pawns } from './pawn';
+import { GraphService } from '../_services/graph/graph.service';
+import * as d3 from 'd3';
+import { rgb } from 'd3';
+
+
+export class thief extends pawns{
+    role: any;
+    constructor(private graphServ: GraphService){
+        super(graphServ)
+        d3.select("svg")
+        .append('circle')
+        //.data(this)
+        //.join("circle")
+            .datum(function(){ return this; })
+            .attr("class", "pawns")
+            .attr("cx", this.x)
+            .attr("cy", this.y)
+            .attr("r", this.radius)
+            .attr("fill", "rgb(0,255,0)")
+            .call(d3.drag()
+                .on("start", this.dragstarted.bind(this))
+                .on("drag", this.dragged)
+                .on("end", this.dragended.bind(this)));
+    }
+}
