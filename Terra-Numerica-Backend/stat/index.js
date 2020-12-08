5c708562d312ae6beef7be7840d97a80f939c4b6
Gregory-Hoareau <43096860+Gregory-Hoareau@users.noreply.github.com>
2020-12-08 15:38:15 +0100

    #16 Ajout des routes pour les statistiques

--- Terra-Numerica-Backend/stat/index.js
+++ Terra-Numerica-Backend/stat/index.js
@@ -1,20 +1,20 @@
 const {Router} = require('express');
-const graphs = require('./graphs.json');
+const stats = require('./stats.json');
 const RestOperator = require('../utils/rest-operator')
 
 //Configuration du RestOperator
-RestOperator.setName('graph');
-RestOperator.setFilename('graph/graphs.json');
+RestOperator.setName('stat');
+RestOperator.setFilename('stat/stats.json');
 RestOperator.load();
 
 const router = new Router();
 router.get('/', (req, res) => {
     res.status(200).json(RestOperator.get());
 });
-router.get('/:graphId', (req, res) => {
+router.get('/:statId', (req, res) => {
     try {
-        const graph = RestOperator.getById(req.params.graphId);
-        res.status(200).json(graph);
+        const stat = RestOperator.getById(req.params.statId);
+        res.status(200).json(stat);
     } catch (e) {
         if(e.name === 'ItemNotFoundError') {
             res.status(404).end();
@@ -25,16 +25,16 @@ router.get('/:graphId', (req, res) => {
 });
 router.post('/', (req, res) => {
     try {
-        const graph = RestOperator.post(req.body);
-        res.status(201).json(graph);
+        const stat = RestOperator.post(req.body);
+        res.status(201).json(stat);
     } catch (e) {
         res.status(500).json(e);
     }
 });
-router.put('/:graphId', (req, res) => {
+router.put('/:statId', (req, res) => {
     try {
-        const graph = RestOperator.update(req.params.graphId, req.body);
-        res.status(200).json(graph);
+        const stat = RestOperator.update(req.params.statId, req.body);
+        res.status(200).json(stat);
     } catch (e) {
         if(e.name === 'ItemNotFoundError') {
             res.status(404).end();
@@ -43,9 +43,9 @@ router.put('/:graphId', (req, res) => {
         }
     }
 });
-router.delete('/:graphId', (req, res) => {
+router.delete('/:statId', (req, res) => {
     try {
-        const graph = RestOperator.delete(req.params.graphId);
+        const stat = RestOperator.delete(req.params.statId);
         res.status(204).end();
     } catch (e) {
         if(e.name === 'ItemNotFoundError') {
