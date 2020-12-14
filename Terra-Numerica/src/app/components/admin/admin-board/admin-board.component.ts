02d7ca4c355b02d8522e7a22961908b1ae3a0ebe
Gregory-Hoareau <43096860+Gregory-Hoareau@users.noreply.github.com>
2020-12-14 12:35:06 +0100

    #28 Ajout d'un mot de passe d'accès
    -On ne peut accèder au panneau d'administration que si l'on connait le mot de passe le permettant

--- Terra-Numerica/src/app/components/admin/admin-board/admin-board.component.ts
+++ Terra-Numerica/src/app/components/admin/admin-board/admin-board.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal, { SweetAlertOptions } from 'sweetalert2';
 
 @Component({
   selector: 'app-admin-board',
@@ -13,9 +15,12 @@ export class AdminBoardComponent implements OnInit {
 
   public type = this.ADD_GRAPH; // possible value are: 'dashboard', 'add-graph'
 
-  constructor() { }
+  private PASSWORD = 'admin';
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.tryToAccess();
   }
 
   selectDisplay(type) {
@@ -24,6 +29,46 @@ export class AdminBoardComponent implements OnInit {
 
   isSelected(type) {
     return this.type === type ? 'selected' : '';
+  }
+
+  private tryToAccess(error = false) {
+    let options: SweetAlertOptions = {
+      title: 'Accès au panneau d\'administration.',
+      text: 'Veuillez saissir le mot de passe pour accèder au panneau d\'administration du site.',
+      input: 'password',
+      inputPlaceholder: 'Mot de passe',
+      confirmButtonText: 'Valider',
+      confirmButtonColor: 'purple',
+      showCancelButton: true,
+      cancelButtonText: 'Retour au menu',
+      cancelButtonColor: 'rgb(190,0,0)',
+      allowOutsideClick: false
+    }
+    if(error) 
+      options.footer = '<span style="color: red;">Le mot de passe saissi est incorrect.</span>';
+
+    Swal.fire(options).then((result) => {
+      console.log(result)
+      if(result.isConfirmed) {
+        if(!this.checkPassword(result.value)) {
+          this.tryToAccess(true)
+        } else {
+          Swal.fire('Accès autorisé', '', 'success')
+        }
+      } else if(result.isDismissed || result.isDenied) {
+        this.router.navigate(['/'])
+      }
+    })
+  }
+
+  /**
+   * Cette fonction vérifier si le mot de passe passer en paramètre est correct. Si le mot de passe est correct la fonction renvoie 'true'
+   * sinon la fonction renvoie false
+   * 
+   * @param pwd mot de passe saissie par l'utilisateur
+   */
+  private checkPassword(pwd) {
+    return pwd === this.PASSWORD;
   }
 
 }
