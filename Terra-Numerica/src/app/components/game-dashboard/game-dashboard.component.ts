1a520464c2f7a3a40e6691710dcd2487c4d3f41c
AnthonyChoquard <anthony.choquard@etu.univ-cotedazur.fr>
2020-12-16 14:55:53 +0100

    fix suppression des doublon (titres et bouton close) sur les graphes stat #28

--- Terra-Numerica/src/app/components/game-dashboard/game-dashboard.component.ts
+++ Terra-Numerica/src/app/components/game-dashboard/game-dashboard.component.ts
@@ -38,21 +38,6 @@ export class GameDashboardComponent implements OnInit {
       .style("flex-direction", "column")
       .style("align-items", "stretch")
       .style("height", "50vh");
-    card.append("button")
-      .style("width", "5%")
-      .attr("fill", "red")
-      .on("click", () => {
-        card.remove()
-      })
-      .append("img")
-        .attr("src", "assets/close.svg")
-        .style("width", "100%")
-
-    card.append("div")
-      .style("text-align", "center")
-      .append("h2")
-        .text(this.abcisse + " en fonction du " + this.ordonne)
-        .style("margin-bottom", 0)
 
     const title = card.append("div")
     .style("text-align", "center")
