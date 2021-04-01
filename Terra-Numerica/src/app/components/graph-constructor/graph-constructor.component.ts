375e290ca1de53a29b7c8102a2bac09e77750efa
Gregory-Hoareau <66921801+Gregory-Hoareau@users.noreply.github.com>
2021-04-01 12:19:17 +0200

    #29 Ajout du .htaccess au build l'application

--- Terra-Numerica/src/app/components/graph-constructor/graph-constructor.component.ts
+++ Terra-Numerica/src/app/components/graph-constructor/graph-constructor.component.ts
@@ -34,10 +34,10 @@ export class GraphConstructorComponent implements OnInit {
     this.selected_tool = this.tools[0];
     d3.select('#canvas')
       .append('svg')
-        .attr('id', this.graphConstructorID)
-        .attr('width', '100%')
-        .attr('height', '100%')
-        .on('click', this.toolAction.bind(this))
+      .attr('id', this.graphConstructorID)
+      .attr('width', '100%')
+      .attr('height', '100%')
+      .on('click', this.toolAction.bind(this))
     this.graphConstructorSVG = d3.select(`#${this.graphConstructorID}`);
     this.lineLayer = this.graphConstructorSVG.append('g').attr('id', 'lineLayer');
     this.circleLayer = this.graphConstructorSVG.append('g').attr('id', 'circleLayer')
@@ -59,27 +59,27 @@ export class GraphConstructorComponent implements OnInit {
   drawNode(position) {
     this.graphConstructorService.toolAction(this.selected_tool, position)
     this.circleLayer.append('circle')
-                              .attr('r', 20)
-                              .attr('class', 'circle')
-                              .style('fill', this.graphConstructorService.originalNodeColor)
-                              .attr('cx', position.x)
-                              .attr('cy', position.y)
-                              .style('z-index', 1)
-                              .call(
-                                d3.drag()
-                                  .on('start', (event: DragEvent) => {
-                                    this.dragstarted(event)
-                                  })
-                                  .on('drag', (event: DragEvent) => {
-                                    this.dragged(event)
-                                  })
-                                  .on('end', (event: DragEvent) => {
-                                    this.dragended(event)
-                                  })
-                              )
-                              .on('click', (event: MouseEvent) => {
-                                this.handleClickOnNode(event.target)
-                              })
+      .attr('r', 20)
+      .attr('class', 'circle')
+      .style('fill', this.graphConstructorService.originalNodeColor)
+      .attr('cx', position.x)
+      .attr('cy', position.y)
+      .style('z-index', 1)
+      .call(
+        d3.drag()
+          .on('start', (event: DragEvent) => {
+            this.dragstarted(event)
+          })
+          .on('drag', (event: DragEvent) => {
+            this.dragged(event)
+          })
+          .on('end', (event: DragEvent) => {
+            this.dragended(event)
+          })
+      )
+      .on('click', (event: MouseEvent) => {
+        this.handleClickOnNode(event.target)
+      })
   }
 
   private handleClickOnNode(circle) {
@@ -87,9 +87,9 @@ export class GraphConstructorComponent implements OnInit {
       case 'add-link':
         if (!this.placing_link) {
           circle.style.fill = this.graphConstructorService.selectedNodeColor;
-          this.from = circle 
+          this.from = circle
         } else {
-          if(this.from !== circle) {
+          if (this.from !== circle) {
             this.drawLink(this.from, circle);
           }
           this.from = null;
@@ -105,7 +105,7 @@ export class GraphConstructorComponent implements OnInit {
 
   private removeCircle(circle) {
     const toDelete = this.links.filter(link => link.source === circle || link.target === circle).map(link => link.line);
-    for(const l of toDelete) {
+    for (const l of toDelete) {
       this.removeLine(l)
     }
     d3.select(circle).remove();
@@ -114,7 +114,7 @@ export class GraphConstructorComponent implements OnInit {
 
   private removeLine(line) {
     let l = this.links.find(link => link.line === line);
-    l = {source: this.convertCircleToPosition(l.source), target: this.convertCircleToPosition(l.target)}
+    l = { source: this.convertCircleToPosition(l.source), target: this.convertCircleToPosition(l.target) }
     this.links = this.links.filter(link => link.line !== line);
     line.remove();
     this.graphConstructorService.toolAction(this.selected_tool, l.source, l.target)
@@ -129,25 +129,25 @@ export class GraphConstructorComponent implements OnInit {
     const toPosition = this.convertCircleToPosition(to);
     this.graphConstructorService.toolAction(this.selected_tool, fromPosition, toPosition)
     const line = this.lineLayer.append('line')
-                                          .attr('class', 'line')
-                                          .attr('stroke', '#aaa')
-                                          .attr('stroke-width', 3)
-                                          .attr('x1', fromPosition.x)
-                                          .attr('y1', fromPosition.y)
-                                          .attr('x2', toPosition.x)
-                                          .attr('y2', toPosition.y)
-                                          .on('click', (event: MouseEvent) => {
-                                            this.handleClickOnLink(event.target)
-                                          })
+      .attr('class', 'line')
+      .attr('stroke', '#aaa')
+      .attr('stroke-width', 3)
+      .attr('x1', fromPosition.x)
+      .attr('y1', fromPosition.y)
+      .attr('x2', toPosition.x)
+      .attr('y2', toPosition.y)
+      .on('click', (event: MouseEvent) => {
+        this.handleClickOnLink(event.target)
+      })
     this.links.push({ source: from, target: to, line: line._groups[0][0] })
   }
 
   private convertCircleToPosition(circle) {
-    return {x: circle.cx.baseVal.value, y: circle.cy.baseVal.value}
+    return { x: circle.cx.baseVal.value, y: circle.cy.baseVal.value }
   }
 
   private handleClickOnLink(line) {
-    if(this.selected_tool === 'remove') {
+    if (this.selected_tool === 'remove') {
       this.removeLine(line);
     }
   }
@@ -188,7 +188,7 @@ export class GraphConstructorComponent implements OnInit {
 
   // Drag & Drop Functions
   dragstarted(event) {
-    if(this.selected_tool === 'move') {
+    if (this.selected_tool === 'move') {
       this.movingCircleOriginalPosition = {
         x: event.sourceEvent.target.cx.baseVal.value,
         y: event.sourceEvent.target.cy.baseVal.value
@@ -198,13 +198,13 @@ export class GraphConstructorComponent implements OnInit {
   }
 
   dragged(event) {
-    if(this.selected_tool === 'move') {
+    if (this.selected_tool === 'move') {
       const circle = event.sourceEvent.target
       d3.select(circle).raise().attr("cx", event.x).attr("cy", event.y);
       this.links.forEach(link => {
-        if(link.source === circle) {
+        if (link.source === circle) {
           d3.select(link.line).attr('x1', event.x).attr('y1', event.y)
-        } else if(link.target === circle) {
+        } else if (link.target === circle) {
           d3.select(link.line).attr('x2', event.x).attr('y2', event.y)
         }
       })
@@ -212,7 +212,7 @@ export class GraphConstructorComponent implements OnInit {
   }
 
   dragended(event) {
-    if(this.selected_tool === 'move') {
+    if (this.selected_tool === 'move') {
       const circle = d3.select(event.sourceEvent.target)
       circle.attr('stroke', null);
       const endPositon = {
