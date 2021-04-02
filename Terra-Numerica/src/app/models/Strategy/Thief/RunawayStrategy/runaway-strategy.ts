a4f7ecf2eb9b4c80461239a0677afebabb1844f3
Gregory-Hoareau <66921801+Gregory-Hoareau@users.noreply.github.com>
2021-04-02 15:50:10 +0200

    #34 Placement sur les sommets les plus loin des policiers

--- Terra-Numerica/src/app/models/Strategy/Thief/RunawayStrategy/runaway-strategy.ts
+++ Terra-Numerica/src/app/models/Strategy/Thief/RunawayStrategy/runaway-strategy.ts
@@ -9,7 +9,33 @@ export class RunawayStrategy implements IStrategy {
 
     placement(graph: Graph, cops_position_slot: any[], thiefs_position_slot: any[]) {
         this.actual_place = graph.getRandomEdge();
-        return this.actual_place;
+        let max_dist = -1;
+        let distant_edges = [];
+        let toCheck = [];
+        toCheck.push(this.actual_place)
+        const marked = [];
+        while (!toCheck.every((edge) => marked.includes(edge))) {
+            const tmp = toCheck;
+            toCheck = []
+            for(const edge of tmp) {
+                let distance = 0;
+                for(const position of cops_position_slot) {
+                    distance += graph.distance(edge, position)
+                }
+                if(distance > max_dist) {
+                    max_dist = distance;
+                    distant_edges = [edge]
+                } else if(distance === max_dist) {
+                    distant_edges.push(edge)
+                }
+                marked.push(edge)
+                graph.edges(edge).forEach(e => {
+                    toCheck.push(e);
+                })
+            }
+        }
+        const res = distant_edges[Math.floor(Math.random() * Math.floor(distant_edges.length))]
+        return res;
     }
 
     move(graph: Graph, cops_position_slot: any[], thiefs_position_slot: any[]) {
